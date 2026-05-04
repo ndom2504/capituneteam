1a24646c6811c11b6a01f6f294a0e985aedf934f
ndom2504 <ndom2504@gmail.com>
2026-05-04 16:53:42 -0400

    Fix CORS to allow www.capitune.com

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,19 @@ import communityRoutes from './routes/community.js';
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      process.env.FRONTEND_URL,
+      'http://localhost:3000',
+      'https://capitune.com',
+      'https://www.capitune.com'
+    ].filter(Boolean);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
 
